b92f82fc9e2e08f0b0df0a9348a2222117e31f6b
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-06-30 21:35:38 -0300

    add base entity

---
+++ src/main/java/com/mza_agrotours/backend/entities/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.mza_agrotours.backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+}
