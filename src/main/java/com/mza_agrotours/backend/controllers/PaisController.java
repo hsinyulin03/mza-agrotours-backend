039e3067077949a56378c25c9cfddfc6724a577c
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-07-11 13:52:19 -0300

    fix: cambiar retorno de getPaises por ResponseEntity

--- src/main/java/com/mza_agrotours/backend/controllers/PaisController.java
+++ src/main/java/com/mza_agrotours/backend/controllers/PaisController.java
@@ -1,7 +1,9 @@
 package com.mza_agrotours.backend.controllers;
 
+import com.mza_agrotours.backend.dtos.ApiResponse;
 import com.mza_agrotours.backend.dtos.PaisGetDTO;
 import com.mza_agrotours.backend.services.PaisService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,8 @@ public class PaisController {
 
 
     @GetMapping("/")
-    public List<PaisGetDTO> getPaises() {
-        return this.paisService.getPaisesDTO();
+    public ResponseEntity<?> getPaises() {
+        ApiResponse<List<PaisGetDTO>> response = ApiResponse.ok(this.paisService.getPaisesDTO());
+        return ResponseEntity.ok(response);
     }
 }
