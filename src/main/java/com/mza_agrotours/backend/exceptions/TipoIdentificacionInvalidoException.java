503f6ad8965818bd6d1f0c77773faa3355b9dc6d
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-07-11 12:24:22 -0300

    feat: añadir seeder de tipo identificacion

---
+++ src/main/java/com/mza_agrotours/backend/exceptions/TipoIdentificacionInvalidoException.java
@@ -0,0 +1,7 @@
+package com.mza_agrotours.backend.exceptions;
+
+public class TipoIdentificacionInvalidoException extends RuntimeException {
+    public TipoIdentificacionInvalidoException(String message) {
+        super(message);
+    }
+}
