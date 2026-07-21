a07cf2377cffabff1a93a05e9a26d3f5efe987e6
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-07-21 15:15:13 -0300

    feat: add query to fetch reservations by visitante and estado

--- src/main/java/com/mza_agrotours/backend/repositories/ReservaRepository.java
+++ src/main/java/com/mza_agrotours/backend/repositories/ReservaRepository.java
@@ -1,8 +1,15 @@
 package com.mza_agrotours.backend.repositories;
 
 import com.mza_agrotours.backend.entities.reservas.Reserva;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface ReservaRepository extends BaseEntityRepository<Reserva, UUID> {
+    @Query("select r from Reserva r where r.visitante.id = :visitanteId and r.estadoActual.estadoReserva.id = :estadoId")
+    List<Reserva> findByVisitanteAndReservaEstadoActual(@Param("visitanteId") UUID visitanteId, @Param("estadoId") UUID estadoReservaId);
 }
