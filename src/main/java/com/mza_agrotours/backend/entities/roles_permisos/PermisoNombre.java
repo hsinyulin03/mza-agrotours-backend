dc761b1c602c3a1c679239bd3bf8b13742dc8656
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-07-30 14:38:32 -0300

    feat create permiso and tipopermiso enums

---
+++ src/main/java/com/mza_agrotours/backend/entities/roles_permisos/PermisoNombre.java
@@ -0,0 +1,13 @@
+package com.mza_agrotours.backend.entities.roles_permisos;
+
+public enum PermisoNombre {
+    // Administrador
+    CREAR_ADMIN,
+    MODIFICAR_ADMIN,
+    ELIMINAR_ADMIN,
+
+    // Productor
+    CREAR_PRODUCTOR,
+    MODIFICAR_PRODUCTOR,
+    ELIMINAR_PRODUCTOR,
+}
---
+++ src/main/java/com/mza_agrotours/backend/entities/roles_permisos/TipoPermisoNombre.java
@@ -0,0 +1,7 @@
+package com.mza_agrotours.backend.entities.roles_permisos;
+
+public enum TipoPermisoNombre {
+    ADMIN,
+    VISITANTE,
+    PRODUCTOR
+}
