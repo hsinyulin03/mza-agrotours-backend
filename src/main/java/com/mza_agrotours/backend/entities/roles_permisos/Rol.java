d64a73c6975be6f6a1c44be5b8f89747236c7a3c
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-07-30 14:39:51 -0300

    feat: create rol entity

---
+++ src/main/java/com/mza_agrotours/backend/entities/roles_permisos/Rol.java
@@ -0,0 +1,40 @@
+package com.mza_agrotours.backend.entities.roles_permisos;
+
+import com.mza_agrotours.backend.entities.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Rol extends BaseEntity {
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String descripcion;
+
+    private LocalDateTime fechaHoraBaja;
+
+    @Column(nullable = false)
+    private Boolean esProtegido;
+
+    @ManyToMany
+    @JoinTable(
+            name = "rol_permiso",
+            joinColumns = @JoinColumn(name = "rol_id"),
+            inverseJoinColumns = @JoinColumn(name = "permiso_id")
+    )
+    private List<Permiso> permisos;
+
+    @ManyToOne(optional = false)
+    private TipoPermiso tipoPermiso;
+}
