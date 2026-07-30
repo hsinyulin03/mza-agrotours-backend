5bcec32543dfb4b6ee28bec1822783625cb2b57a
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-07-30 14:38:45 -0300

    feat: create tipo permiso

---
+++ src/main/java/com/mza_agrotours/backend/entities/roles_permisos/TipoPermiso.java
@@ -0,0 +1,20 @@
+package com.mza_agrotours.backend.entities.roles_permisos;
+
+import com.mza_agrotours.backend.entities.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TipoPermiso extends BaseEntity {
+    @Enumerated(EnumType.STRING)
+    private TipoPermisoNombre nombre;
+}
