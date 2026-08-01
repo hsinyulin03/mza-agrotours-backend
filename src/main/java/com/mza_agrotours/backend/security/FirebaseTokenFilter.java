17074705c199b6072e817f70c7e2e32a084c1d16
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-08-01 15:20:28 -0300

    feat: get admin authorities based on email in firebase token

--- src/main/java/com/mza_agrotours/backend/security/FirebaseTokenFilter.java
+++ src/main/java/com/mza_agrotours/backend/security/FirebaseTokenFilter.java
@@ -5,6 +5,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import com.mza_agrotours.backend.dtos.UsuarioAuthDetails;
+import com.mza_agrotours.backend.entities.roles_permisos.Permiso;
+import com.mza_agrotours.backend.services.RolService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,6 +23,12 @@ import java.util.List;
 
 @Component
 public class FirebaseTokenFilter extends OncePerRequestFilter {
+    private final RolService rolService;
+
+    public FirebaseTokenFilter(RolService rolService) {
+        this.rolService = rolService;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String header = request.getHeader("Authorization");
@@ -29,8 +37,8 @@ public class FirebaseTokenFilter extends OncePerRequestFilter {
 
             try {
                 FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
-
-                var authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+                // TODO: split on two filters AdminAuthoritiesFilter and ProductoresAuthoritiesFilter or something
+                var authorities = getAuthorities(decodedToken.getEmail());
 
                 UsuarioAuthDetails usuarioAuthDetails = UsuarioAuthDetails.builder()
                         .email(decodedToken.getEmail())
@@ -45,5 +53,17 @@ public class FirebaseTokenFilter extends OncePerRequestFilter {
             }
         }
         filterChain.doFilter(request, response);
+    }
+
+    private List<SimpleGrantedAuthority> getAuthorities(String email) {
+        try {
+            List<Permiso> permisosEmail = this.rolService.obtenerPermisosAdminPorEmail(email);
+            return permisosEmail
+                    .stream()
+                    .map((p) -> new SimpleGrantedAuthority(p.getNombre().name()))
+                    .toList();
+        } catch (Exception e) {
+            return List.of();
+        }
     }
 }
