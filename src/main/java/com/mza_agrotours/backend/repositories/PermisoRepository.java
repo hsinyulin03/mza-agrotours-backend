44e3ff6deb30dce4751361e37429450b68533d84
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-08-01 15:17:45 -0300

    feat: create permiso repository

--- src/main/java/com/mza_agrotours/backend/repositories/PermisoRepository.java
+++ src/main/java/com/mza_agrotours/backend/repositories/PermisoRepository.java
@@ -2,10 +2,14 @@ package com.mza_agrotours.backend.repositories;
 
 import com.mza_agrotours.backend.entities.roles_permisos.Permiso;
 import com.mza_agrotours.backend.entities.roles_permisos.PermisoNombre;
+import com.mza_agrotours.backend.entities.roles_permisos.TipoPermiso;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PermisoRepository extends BaseEntityRepository<Permiso, UUID> {
     Optional<Permiso> findByNombre(PermisoNombre nombre);
+
+    List<Permiso> findByTipoPermiso(TipoPermiso tipoPermiso);
 }
