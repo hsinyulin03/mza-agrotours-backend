6f1b40c8514f2ef0ea72e8a1f356c08d7aa35852
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-08-08 14:32:08 -0300

    feat: add repository methods

---
+++ src/main/java/com/mza_agrotours/backend/repositories/ProductorRepository.java
@@ -0,0 +1,8 @@
+package com.mza_agrotours.backend.repositories;
+
+import com.mza_agrotours.backend.dtos.productor.Productor;
+
+import java.util.UUID;
+
+public interface ProductorRepository extends BaseEntityRepository<Productor, UUID> {
+}
