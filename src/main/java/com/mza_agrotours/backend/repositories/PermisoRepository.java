eaf0704a976698ac6f77732ab97f924cedd02782
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-08-13 14:30:39 -0300

    feat: add permisoRepository method to find permisos by nombre in list and tipoPermiso

--- src/main/java/com/mza_agrotours/backend/repositories/PermisoRepository.java
+++ src/main/java/com/mza_agrotours/backend/repositories/PermisoRepository.java
@@ -3,6 +3,8 @@ package com.mza_agrotours.backend.repositories;
 import com.mza_agrotours.backend.entities.roles_permisos.Permiso;
 import com.mza_agrotours.backend.entities.roles_permisos.TipoPermiso;
 import com.mza_agrotours.backend.enums.PermisoNombre;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +14,8 @@ public interface PermisoRepository extends BaseEntityRepository<Permiso, UUID> {
     Optional<Permiso> findByNombre(PermisoNombre nombre);
 
     List<Permiso> findByTipoPermiso(TipoPermiso tipoPermiso);
+
+    @Query("select p from Permiso p where p.nombre in :nombres and p.tipoPermiso = :tipoPermiso")
+    List<Permiso> findByTipoPermisoAndNombreIn(@Param("tipoPermiso") TipoPermiso tipoPermiso,
+                                               @Param("nombres") List<String> nombres);
 }
