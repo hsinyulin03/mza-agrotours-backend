9f6ea4ef4adc220cb8dd894347bf71cf10dcb3d4
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-08-14 13:21:48 -0300

    refactor: RolRepository refactor permisoCodigos to match permiso codes

--- src/main/java/com/mza_agrotours/backend/repositories/PermisoRepository.java
+++ src/main/java/com/mza_agrotours/backend/repositories/PermisoRepository.java
@@ -2,7 +2,7 @@ package com.mza_agrotours.backend.repositories;
 
 import com.mza_agrotours.backend.entities.roles_permisos.Permiso;
 import com.mza_agrotours.backend.entities.roles_permisos.TipoPermiso;
-import com.mza_agrotours.backend.enums.PermisoNombre;
+import com.mza_agrotours.backend.enums.PermisoCodigo;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,11 +11,11 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface PermisoRepository extends BaseEntityRepository<Permiso, UUID> {
-    Optional<Permiso> findByNombre(PermisoNombre nombre);
+    Optional<Permiso> findByCodigo(PermisoCodigo codigo);
 
     List<Permiso> findByTipoPermiso(TipoPermiso tipoPermiso);
 
-    @Query("select p from Permiso p where p.nombre in :nombres and p.tipoPermiso = :tipoPermiso")
-    List<Permiso> findByTipoPermisoAndNombreIn(@Param("tipoPermiso") TipoPermiso tipoPermiso,
-                                               @Param("nombres") List<String> nombres);
+    @Query("select p from Permiso p where p.codigo in :codigos and p.tipoPermiso = :tipoPermiso")
+    List<Permiso> findByTipoPermisoAndCodigoIn(@Param("tipoPermiso") TipoPermiso tipoPermiso,
+                                               @Param("codigos") List<String> codigos);
 }
