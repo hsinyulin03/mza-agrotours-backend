141e147742abaf867ddb45b84bba812111b9ba0b
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-09-01 11:59:40 -0300

    feat: add motivo size validation

--- src/main/java/com/mza_agrotours/backend/dtos/administrador_sistemas/EstablecimientoSuspenderReq.java
+++ src/main/java/com/mza_agrotours/backend/dtos/administrador_sistemas/EstablecimientoSuspenderReq.java
@@ -1,6 +1,7 @@
 package com.mza_agrotours.backend.dtos.administrador_sistemas;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,5 +11,6 @@ import lombok.Setter;
 @Setter
 public class EstablecimientoSuspenderReq {
     @NotBlank
+    @Size(max = 200)
     private String motivo;
 }
