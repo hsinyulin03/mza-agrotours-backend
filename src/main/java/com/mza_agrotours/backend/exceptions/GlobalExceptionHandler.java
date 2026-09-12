0edb513235cb26e20d8f393d269b7d2f922f4d53
Hsin <hsinyulin2971@gmail.com>
2026-09-12 14:16:56 -0300

    fix: se agrega un handler para casos donde se cumple con la expresión regular pero firebase lo rechaza, antes firebase lo rechazaba pero en el response aparecía el campo vacío y ahora imprimimos el mensaje para capturar mejor el error

--- src/main/java/com/mza_agrotours/backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/mza_agrotours/backend/exceptions/GlobalExceptionHandler.java
@@ -27,7 +27,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     // --- INICIO EXCEPCIONES USUARIO
@@ -71,6 +71,17 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(FirebaseAuthException.class)
     public ResponseEntity<?> handleFirebaseAuthException(FirebaseAuthException ex) {
+        if (ex.getAuthErrorCode() == null) {
+            String message = ex.getMessage() != null &&
+                    ex.getMessage().contains("INVALID_PHONE_NUMBER")
+                    ? "El teléfono no es válido. Usá formato internacional, ej: +5492611234567"
+                    : "No se pudo completar la operación con el proveedor de autenticación";
+            log.error("Firebase sin AuthErrorCode: {}", ex.getMessage(), ex);
+
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(ApiResponse.fail("firebaseError", message));
+        }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.fail(ex.getAuthErrorCode().toString(), ex.getMessage()));
     }
     @ExceptionHandler(EntityNotFoundException.class)
