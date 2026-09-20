f648c1d8d133b05629d3606b2d03d4e67c6017b3
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-09-20 00:10:16 -0300

    feat: add S3ObjectStorageService for generating presigned URLs and download URLs

---
+++ src/main/java/com/mza_agrotours/backend/services/S3ObjectStorageService.java
@@ -0,0 +1,94 @@
+package com.mza_agrotours.backend.services;
+
+import com.mza_agrotours.backend.config.ObjectStorageProperties;
+import com.mza_agrotours.backend.dtos.archivo.PresignedUrlRequest;
+import com.mza_agrotours.backend.dtos.archivo.PresignedUrlResponse;
+import com.mza_agrotours.backend.exceptions.DatoInvalidoException;
+import com.mza_agrotours.backend.exceptions.ObjectStorageProviderException;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+/**
+ * Urls prefirmadas contra un storage compatible con S3: MinIO en desarrollo y
+ * en los tests, AWS S3 en produccion. Los bytes nunca pasan por el backend.
+ */
+@Service
+public class S3ObjectStorageService {
+    private final S3Presigner presigner;
+    private final ObjectStorageProperties properties;
+
+    public S3ObjectStorageService(S3Presigner presigner, ObjectStorageProperties properties) {
+        this.presigner = presigner;
+        this.properties = properties;
+    }
+
+    /**
+     * @throws DatoInvalidoException si el tamanio declarado supera el maximo
+     * @throws ObjectStorageProviderException si el proveedor no pudo firmar la url
+     */
+    public PresignedUrlResponse generatePresignedUrl(PresignedUrlRequest request)
+            throws ObjectStorageProviderException {
+        if (request.getFileSize() > this.properties.getMaxFileSize()) {
+            throw new DatoInvalidoException("El archivo " + request.getFilename()
+                    + " supera el tamanio maximo permitido de " + this.properties.getMaxFileSize() + " bytes");
+        }
+
+        String key = ObjectStorageKeys.generate(request.getFilename());
+        String contentType = contentTypeOf(key);
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(this.properties.getBucket())
+                .key(key)
+                .contentType(contentType)
+                .build();
+
+        try {
+            String uploadUrl = this.presigner.presignPutObject(PutObjectPresignRequest.builder()
+                            .signatureDuration(this.properties.getPresignTtl())
+                            .putObjectRequest(putObjectRequest)
+                            .build())
+                    .url()
+                    .toString();
+
+            return new PresignedUrlResponse(uploadUrl, key, contentType);
+        } catch (SdkException e) {
+            throw new ObjectStorageProviderException(e.getMessage(), e.getClass().getSimpleName());
+        }
+    }
+
+    public String generateDownloadUrl(String key) {
+        if (!ObjectStorageKeys.isValid(key)) {
+            throw new DatoInvalidoException("Key de objeto invalida: " + key);
+        }
+
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(this.properties.getBucket())
+                .key(key)
+                .build();
+
+        return this.presigner.presignGetObject(GetObjectPresignRequest.builder()
+                        .signatureDuration(this.properties.getPresignTtl())
+                        .getObjectRequest(getObjectRequest)
+                        .build())
+                .url()
+                .toString();
+    }
+
+    /**
+     * El content type lo decide el servidor a partir de la extension y va
+     * firmado en la url: el cliente no puede guardar un objeto declarando un
+     * tipo que no le corresponde.
+     */
+    private String contentTypeOf(String key) {
+        return MediaTypeFactory.getMediaType(key)
+                .orElse(MediaType.APPLICATION_OCTET_STREAM)
+                .toString();
+    }
+}
